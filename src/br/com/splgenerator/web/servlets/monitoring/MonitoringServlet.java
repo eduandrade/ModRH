b4cff8fba407fd9a0527aa06f0393934df5fcd83
Eduardo <edu.p.andrade@gmail.com>
2014-05-05 19:14:37 -0300

    monitoring factory

--- src/br/com/splgenerator/web/servlets/monitoring/MonitoringServlet.java
+++ src/br/com/splgenerator/web/servlets/monitoring/MonitoringServlet.java
@@ -8,20 +8,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.splgenerator.web.servlets.monitoring.xml.ModRhStatsXml;
+import br.com.splgenerator.web.servlets.monitoring.MonitoringFactory.MonitoringType;
 
 @WebServlet("/monitoring")
 public class MonitoringServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-	
-    public MonitoringServlet() {
-        super();
-    }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//IModRhStats stats = new ModRhStatsTxt();
-		IModRhStats stats = new ModRhStatsXml();
+		IModRhStats stats = MonitoringFactory.getMonitoring(MonitoringType.XML);
 		response.getWriter().println(stats.printStats());
 	}
 
